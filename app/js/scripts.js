0159de70b23fb69e1b6b7c806fab2d3499821d3c
Jim Mathes <jimmathes@Jims-MBP.fritz.box>
2020-10-27 12:36:55 +0100

    Final Commit after finishing the app

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,18 +1,8 @@
 
-// import CreateContactLayer from './modules/CreateContactLayer';
-// import ContactCreator from './modules/ContactCreator';
-// import ContactDatabase from './modules/ContactDatabase';
-
-
-// var createContactLayer = new CreateContactLayer();
-// var contactCreator = new ContactCreator();
-// var contactDatabase = new ContactDatabase();
-
-
 // CONTACT CONTROLLER
 var contactController = (function(){
 
-        var Contact = function(id, firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth){
+        var Contact = function(id, firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth, companyTitle){
             this.id = id,
             this.firstName = firstName;
             this.lastName = lastName;
@@ -22,6 +12,7 @@ var contactController = (function(){
             this.email = email;
             this.phone = phone;
             this.dateOfBirth = dateOfBirth;
+            this.companyTitle = companyTitle;
 
         };
         
@@ -29,7 +20,7 @@ var contactController = (function(){
         var currentID;
 
         return {
-            addContact: function(firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth) {
+            addContact: function(firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth, companyTitle) {
                 var newContact;
 
                 // create new ID
@@ -40,11 +31,11 @@ var contactController = (function(){
                 }
 
                 // create new item
-                newContact = new Contact(id, firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth);
+                newContact = new Contact(id, firstName, lastName, adress, zipCode, country, email, phone, dateOfBirth, companyTitle);
+
                 //push to List
-                contactList.push(newContact);
-                console.log(contactList);
-            
+                contactList.push(newContact);         
+
                 //return new element
 
                 return newContact;
@@ -52,9 +43,6 @@ var contactController = (function(){
 
             deleteContact: function(id) {
                 var ids, index;
-
-                console.log("Vor dem Löschprozess" + contactList)
-
 
                 ids = contactList.map(function(current){
                     return current.id;
@@ -68,6 +56,8 @@ var contactController = (function(){
                 }
 
             },
+
+
 
             setCurrentID: function(id) {
                 currentID = id;
@@ -107,7 +97,9 @@ var UIController = (function() {
         email: '.email',
         phone: '.phone',
         dateOfBirth: '.date',
+        companyTitle: '.company',
         cardTitle: '.cardTitle',
+        companySubline: '.companyTitle',
         firstNameCard: '.first_name_cardField',
         lastNameCard: '.last_name_cardField',
         adressCard: '.adress_cardField',
@@ -116,6 +108,7 @@ var UIController = (function() {
         emailCard: '.email_cardField',
         phoneCard: '.phone_cardField',
         dateOfBirthCard: '.date_cardField',
+        companyTitleCard: '.company_cardField',
         modal: '.formcontainer',
         overlay: '.overlay',
         newButton: '.newButton',
@@ -125,7 +118,8 @@ var UIController = (function() {
         buttonRowCard: '.button-row',
         saveButtonCard: '.button_save_cardField',
         deleteButtonCard: '.button_delete_cardField',
-        editButtonCard: '.editButton'
+        editButtonCard: '.editButton',
+        contactCard: '.contactCard'
     }
 
     return {
@@ -139,27 +133,31 @@ var UIController = (function() {
                 country: document.querySelector(DOMstrings.country).value,
                 email: document.querySelector(DOMstrings.email).value,
                 phone: document.querySelector(DOMstrings.phone).value,
-                dateOfBirth: document.querySelector(DOMstrings.dateOfBirth).value
+                dateOfBirth: document.querySelector(DOMstrings.dateOfBirth).value,
+                companyTitle: document.querySelector(DOMstrings.companyTitle).value
             }
         },
 
         reloadContactList: function(arr) {
             var html, newHtml, element;
             element = DOMstrings.contactListContainerUL;
-            // delete all LI 
+            // Delete all LI 
             document.querySelector(element).innerHTML = "";
-            // for each in contactlist add Li
 
-            arr.forEach(function(current, index, array){
-              // ctreate HTML string
-              html = '<li class="contactListItemCell" id="%id%"><strong>%lastName%</strong> %firstName%</li>';
-              // Replace Placeholder
-              newHtml = html.replace('%firstName%', arr[index].firstName);
-              newHtml = newHtml.replace('%lastName%', arr[index].lastName);
-              newHtml = newHtml.replace('%id%', arr[index].id);
-              // Insert HTML
-              document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-            });
+            if (arr.length > 0) {
+                arr.forEach(function(current, index, array){
+                // ctreate HTML string
+                html = '<li class="contactListItemCell" id="%id%"><strong>%lastName%</strong> %firstName%</li>';
+                // Replace Placeholder
+                newHtml = html.replace('%firstName%', arr[index].firstName);
+                newHtml = newHtml.replace('%lastName%', arr[index].lastName);
+                newHtml = newHtml.replace('%id%', arr[index].id);
+                // Insert HTML
+                document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+                });
+             } else {
+                document.querySelector(DOMstrings.contactCard).classList.remove('isVisible');
+             }
             
         },
 
@@ -175,26 +173,53 @@ var UIController = (function() {
 
         clearFields: function() {
             var fields, fieldsArr;
-            fields = document.querySelectorAll(DOMstrings.firstName + ' , ' + DOMstrings.lastName + ' , ' + DOMstrings.adress + ' , ' + DOMstrings.zipCode + ' , ' + DOMstrings.country + ' , ' + DOMstrings.email + ' , ' + DOMstrings.phone + ' , ' + DOMstrings.dateOfBirth);
-            
-            fieldsArr = Array.prototype.slice.call(fields);
 
+            fields = document.querySelectorAll(DOMstrings.firstName + ' , ' + DOMstrings.lastName + ' , ' + DOMstrings.adress + ' , ' + DOMstrings.zipCode + ' , ' + DOMstrings.country + ' , ' + DOMstrings.email + ' , ' + DOMstrings.phone + ' , ' + DOMstrings.dateOfBirth + ' , ' + DOMstrings.companyTitle);
+            fieldsArr = Array.prototype.slice.call(fields);
             fieldsArr.forEach(function(current, index, array){
-                current.value = "";
+            current.value = "";
             });
 
             fieldsArr[0].focus();
         },
 
-        enableEditingMode: function() {
-            document.querySelector(DOMstrings.buttonRowCard).classList.add('isVisibleFlex');
+        disableCardFields: function(bool) {
+            var cardFields, cardFieldsArr;
+
+            cardFields = document.querySelectorAll(DOMstrings.firstNameCard + ' , ' + DOMstrings.lastNameCard + ' , ' + DOMstrings.adressCard + ' , ' + DOMstrings.zipCodeCard + ' , ' + DOMstrings.countryCard + ' , ' + DOMstrings.emailCard + ' , ' + DOMstrings.phoneCard + ' , ' + DOMstrings.dateOfBirthCard + ' , ' + DOMstrings.companyTitleCard);
+            cardFieldsArr = Array.prototype.slice.call(cardFields);
+            cardFieldsArr.forEach(function(current, index, array){
+            current.disabled = bool;
+            });
+        },
+
+        enableEditingMode: function(bool) {
+
+            if (bool === true) {
+                document.querySelector(DOMstrings.buttonRowCard).classList.add('isVisibleFlex');
+                document.querySelector(DOMstrings.editButtonCard).style.display = "none";
+                this.disableCardFields(false);
+                } else {
+                document.querySelector(DOMstrings.buttonRowCard).classList.remove('isVisibleFlex');
+                document.querySelector(DOMstrings.editButtonCard).style.display = "block";
+                this.disableCardFields(true);
+            }
         },
 
         displayContact: function(array, index) {
+
             // Remove Editing Mode if true
             document.querySelector(DOMstrings.buttonRowCard).classList.remove('isVisibleFlex');
+            
+            // Show Contact Card
+            document.querySelector(DOMstrings.contactCard).classList.add('isVisible');
+
+            // Disable Fields
+
+            this.disableCardFields(true);
 
             document.querySelector(DOMstrings.cardTitle).innerHTML = array[index].firstName + " " + array[index].lastName;
+            document.querySelector(DOMstrings.companySubline).innerHTML = array[index].companyTitle;
             document.querySelector(DOMstrings.firstNameCard).value = array[index].firstName;
             document.querySelector(DOMstrings.lastNameCard).value = array[index].lastName;
             document.querySelector(DOMstrings.adressCard).value = array[index].adress;
@@ -203,6 +228,20 @@ var UIController = (function() {
             document.querySelector(DOMstrings.emailCard).value = array[index].email;
             document.querySelector(DOMstrings.phoneCard).value = array[index].phone;
             document.querySelector(DOMstrings.dateOfBirthCard).value = array[index].dateOfBirth;
+            document.querySelector(DOMstrings.companyTitleCard).value = array[index].companyTitle;
+        },
+
+        changeContact: function(array, index) {
+            array[index].firstName = document.querySelector(DOMstrings.firstNameCard).value
+            array[index].lastName = document.querySelector(DOMstrings.lastNameCard).value
+            array[index].adress = document.querySelector(DOMstrings.adressCard).value
+            array[index].zipCode = document.querySelector(DOMstrings.zipCodeCard).value
+            array[index].country = document.querySelector(DOMstrings.countryCard).value
+            array[index].email = document.querySelector(DOMstrings.emailCard).value
+            array[index].phone = document.querySelector(DOMstrings.phoneCard).value
+            array[index].dateOfBirth = document.querySelector(DOMstrings.dateOfBirthCard).value
+            array[index].companyTitle = document.querySelector(DOMstrings.companyTitleCard).value
+
         },
 
         getDOMstrings: function() {
@@ -220,12 +259,14 @@ var controller = (function(contactCtrl, UICtrl) {
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings();
 
-        document.querySelector(DOM.newButton).addEventListener('click', UICtrl.openModal);
-        document.querySelector(DOM.closeButton).addEventListener('click', UICtrl.closeModal);
-        document.querySelector(DOM.editButtonCard).addEventListener('click', UICtrl.enableEditingMode);
-        
-        
-        document.querySelector(DOM.deleteButtonCard).addEventListener('click', ctrlDeleteItem);
+        document.querySelector(DOM.newButton).addEventListener('click', UICtrl.openModal); // move to Controller?
+        document.querySelector(DOM.closeButton).addEventListener('click', UICtrl.closeModal); // move to Conntroller?
+
+
+        document.querySelector(DOM.editButtonCard).addEventListener('click', ctrlEditingMode);
+        document.querySelector(DOM.deleteButtonCard).addEventListener('click', ctrldeleteContact);
+        document.querySelector(DOM.saveButtonCard).addEventListener('click', ctrlChangeContact);
+
         document.querySelector(DOM.saveButton).addEventListener('click', ctrlAddContact);
     
         document.addEventListener('keydown', function(event) {
@@ -241,36 +282,30 @@ var controller = (function(contactCtrl, UICtrl) {
     var ctrlAddContact = function() {
         var input, newItem, contactArray;
 
-        ////  1 GET INPUT FROM FIELDS
+        // GET INPUT FROM FIELDS
         input = UICtrl.getInput();
         contactArray = contactController.getContactList();
-        console.log("this is " + contactArray);
 
-        // TODO: 2 ADD THE INPUT TO THE DATASTRUCTURE
+        // ADD THE INPUT TO THE DATASTRUCTURE
 
-        newItem = contactCtrl.addContact(input.firstName, input.lastName, input.adress, input.zipCode, input.country, input.email, input.phone, input.dateOfBirth)
+        newItem = contactCtrl.addContact(input.firstName, input.lastName, input.adress, input.zipCode, input.country, input.email, input.phone, input.dateOfBirth, input.companyTitle)
 
-        // TODO: 3 DISPLAY THE CONTACT IN CONTACTLIST
+        // DISPLAY THE CONTACT IN CONTACTLIST
 
         UICtrl.reloadContactList(contactArray);
-        // UICtrl.clearFields();
-        // UICtrl.closeModal();
+        UICtrl.clearFields();
+        UICtrl.closeModal();
     }
 
     var ctrlDisplayContact = function(event) {
         var itemID, contactArray, contactIndex;
 
         itemID = event.target.id;
-        contactArray = contactController.getContactList(); // FFFFFFEEEEEHHHHLLELRR ??? WARUM
+        contactArray = contactController.getContactList();
         
-        console.log(contactArray);
-
         if (itemID) {
 
             contactIndex = contactController.getContactIndex(itemID);
-
-            console.log("hier ist der array  "  + contactArray);
-            console.log("und hier der passende Index" + contactIndex);
 
             UICtrl.displayContact(contactArray, contactIndex);
 
@@ -278,15 +313,34 @@ var controller = (function(contactCtrl, UICtrl) {
         }
     }
 
-    var ctrlDeleteItem = function() {
+    var ctrlEditingMode = function() {
+
+        UICtrl.enableEditingMode(true);
+
+    }
+
+    var ctrldeleteContact = function() {
         var currentID, contactArray;
-        
+
         currentID = contactCtrl.getCurrentID();
         contactArray = contactController.getContactList();
-
         contactCtrl.deleteContact(currentID)
-
         UICtrl.reloadContactList(contactArray);
+
+    }
+
+    var ctrlChangeContact = function() {
+        var currentID, contactIndex, contactArray;
+
+        currentID = contactCtrl.getCurrentID();
+        contactIndex = contactController.getContactIndex(currentID);
+        contactArray = contactController.getContactList();
+
+        UICtrl.changeContact(contactArray, contactIndex);
+        UICtrl.reloadContactList(contactArray);
+        UICtrl.enableEditingMode(false);
+
+        UICtrl.displayContact(contactArray, contactIndex);
 
     }
 
